79e51be67fc9bd6d461c2c72f17f15a5cfed746d
gael-leaute <138693983+gael-leaute@users.noreply.github.com>
2023-07-15 14:11:05 +0200

    commit whole solution

---
+++ server-client-chat/chat-client/chat-client.cpp
@@ -0,0 +1,117 @@
+﻿// chat-client.cpp : Defines the entry point for the application.
+//
+
+#include <thread>
+#include <iostream>
+#include "chat-client.h"
+#include "rpc/client.h"
+
+using namespace std;
+
+class Service{
+private:
+    bool b_run = true;
+    const std::string exit_cmd = "exit";
+    bool new_outgoing_msg;
+    
+public:
+    std::string message;
+    Service() {
+        new_outgoing_msg = false;
+    }
+
+    bool get_status() {
+        return b_run;
+    }
+
+    void submit(std::string inputLine) {
+        // check for exit_cmd
+        int res = exit_cmd.compare(inputLine);
+        if (0!=res)
+        {
+            message = inputLine;
+            new_outgoing_msg = true;
+        }
+        else
+        {
+            // user request exit_cmd
+            // service shut down
+            b_run = false;
+        }
+    }
+
+    bool new_message() {
+        bool result = new_outgoing_msg;
+        return result;
+    }
+
+    void message_sent() {
+        new_outgoing_msg = false;
+    }
+};
+
+static Service service;
+static rpc::client client("127.0.0.1", rpc::constants::DEFAULT_PORT);
+
+/*
+* Task to receive the messages from the server.
+*/
+void task_receive()
+{
+    client.call("announce", "my_id");
+    std::cout << "client connected" << std::endl;
+
+    while (service.get_status())
+    {
+        auto a_future = client.async_call("msg_callback"); // non-blocking, returns std::future
+
+        string result = a_future.get().as<string>(); // possibly blocks if the result is not yet available
+
+        std::cout << result << std::endl;
+    }
+}
+
+/*
+* Task to send the messages to the server.
+*/
+void task_send()
+{
+    while (service.get_status())
+    {
+        if (true == service.new_message())
+        {
+            auto a_future = client.async_call("broadcast", service.message);
+
+            int result = a_future.get().as<int>(); // possibly blocks if server busy
+            // TBD: result variable could be used to resend message if server timed out
+            // TBD: pass result to message_sent() to mark message to be resend.
+            service.message_sent();
+        }
+    }
+    client.call("exit");
+}
+
+int main() {
+    // launch receive thread
+    std::thread receive_thread(task_receive);
+    // launch send thread
+    std::thread send_thread(task_send);
+
+    std::cout << "type 'exit' to terminate" << std::endl;
+
+    std::string inputLine;
+
+    //loop user input 
+    while (service.get_status()) {
+        // get keaboard line input
+        std::getline(cin, inputLine);
+        // submit the message
+        service.submit(inputLine);
+    }
+
+    // Wait for the threads to finish
+    receive_thread.join();
+    send_thread.join();
+
+    return 0;
+}
---
+++ server-client-chat/chat-server/chat-server.cpp
@@ -0,0 +1,98 @@
+﻿// chat-server.cpp : Defines the entry point for the application.
+//
+
+#include <thread>
+#include "chat-server.h"
+#include "rpc/server.h"
+#include "rpc/this_session.h"
+using namespace std;
+using namespace std::chrono_literals;
+using namespace std::this_thread;
+
+//message data queue with a single queue per client
+//the pair<string,bool>, consists of:
+//string element for the message
+//bool element for the message status
+std::unordered_map<rpc::session_id_t, std::pair<string,bool>> client_data;
+
+void exit_client() {
+    // client requests to disconnect
+    auto id = rpc::this_session().id();
+    std::string exit_msg = "disconnected";
+    // send exit message to close callback handler
+    client_data[id].first = exit_msg;
+    client_data[id].second = true;
+    // wait a second
+    sleep_for(1s);
+    // delete the client from the list
+    client_data.erase(id);
+    std::cout << "client with id " << id << " disconnected." << std::endl;
+    // post exit to the queue
+    rpc::this_session().post_exit();
+}
+
+void announce(std::string const& value) {
+    // a new client requested a connection
+    auto id = rpc::this_session().id();
+    // create welcom message
+    std::string welcome_msg = value;
+    // add client id to the welcome message
+    welcome_msg = welcome_msg + " is:" + to_string(id);
+    // queue in the welcome message
+    client_data[id].first = welcome_msg;
+    client_data[id].second = true;
+    std::cout << "client with id " << id << " annouced." << std::endl;
+}
+
+int broadcast(std::string const& value) {
+    auto id = rpc::this_session().id();
+    // fill the clients message queue
+    for (auto & cd : client_data)
+    {
+        if (cd.first != id) // to not send to the sender
+        {
+            cd.second.first = value;
+            cd.second.second = true;
+        }
+    }
+    return 0;
+}
+
+string msg_handler(void)
+{
+    std::string message;
+    auto id = rpc::this_session().id();
+    // wait until a message in the queue to send
+    while (false == client_data[id].second)
+    {
+
+    }
+    // send the message to the client
+    message = client_data[id].first;
+    client_data[id].second = false;
+    return message;
+}
+
+
+int main() {
+    // Create a server that listens TCP on default port 8080
+    rpc::server srv("0.0.0.0", rpc::constants::DEFAULT_PORT);
+
+    cout << "Server created" << endl;
+
+    srv.bind("msg_callback", &msg_handler);
+
+    srv.bind("exit", &exit_client);
+
+    srv.bind("announce", &announce);
+
+    srv.bind("broadcast", &broadcast);
+
+    constexpr size_t thread_count = 12;
+
+    // Run the server loop.
+    srv.async_run(thread_count); // non-blocking call, handlers execute on one of the workers
+    std::cin.ignore();
+
+    return 0;
+}
